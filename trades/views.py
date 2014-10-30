1a4183e52b7c51dbf06e5875c6859b6625e365ae
Brian Barton <brianjob@umich.edu>
2014-10-29 20:16:47 -0400

    yql fix

--- trades/views.py
+++ trades/views.py
@@ -10,6 +10,7 @@ from trades.helpers import involved_in_trade, is_proposer, is_receiver
 from django.db.models import Q
 import yahoo.application
 import yahoo.oauth
+import yahoo.yql
 	
 # Yahoo! OAuth Credentials - http://developer.yahoo.com/dashboard/
 CONSUMER_KEY      = 'dj0yJmk9Rm11YUJIWGdTcElOJmQ9WVdrOVpYUjZkWFUyTXpRbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD01MQ--'
@@ -297,8 +298,6 @@ def callback(request):
 	oauthapp.token = access_token
 
 	query = "select * from fantasysports.leagues where league_key='328.l.5940'"
-
-	import yahoo.yql
 
 	response = yahoo.yql.YQLQuery().execute(query)
 	if 'query' in response and 'results' in response['query']:
