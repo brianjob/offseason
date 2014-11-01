bdf2f95a1e6c498cfb8b32109f396eafc61f53e5
Brian Barton <brianjob@umich.edu>
2014-11-01 01:02:54 -0400

    pattern -> patterns

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url
 from account import views
 
-urlpattern = patterns('account.views',
+urlpatterns = patterns('account.views',
 
 	url(r'^authenticate_yahoo_user$', views.authenticate_yahoo_user, name='authenticate_yahoo_user'),
     url(r'^callback$', views.callback, name='callback')
