013d895160f9d38b589de9f81685cb50c911e881
Brian Barton <brianjob@umich.edu>
2014-11-01 17:37:01 -0400

    syntax error in urls.py

--- account/urls.py
+++ account/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('account.views',
 
 	url(r'^importleague$', views.import_league, name='import_league'),
     url(r'^callback$', views.callback, name='callback'),
-    url(r'^newleague$', views.new_league, name='new_league')
+    url(r'^newleague$', views.new_league, name='new_league'),
     url(r'^configureinvites$', views.configure_invites, name='configure_invites')
 )
