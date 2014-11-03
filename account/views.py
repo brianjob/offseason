19e981b72225fe1466805f53a0c88c79af61060a
Brian Barton <brianjob@umich.edu>
2014-11-02 21:48:15 -0500

    derp

--- account/views.py
+++ account/views.py
@@ -6,7 +6,6 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate
 from django.core.urlresolvers import reverse
-from offseason.models import Message
 
 IMPORT_LEAGUE_CALLBACK = 'http://intense-retreat-2626.herokuapp.com/account/import_league_callback'
 LINK_PROFILE_CALLBACK = 'http://intense-retreat-2626.herokuapp.com/account/link_profile_callback'
@@ -19,22 +18,7 @@ def register_page(request):
 	return render(request, 'account/register.html')
 
 def register(request):
-	email = request.POST['email']
-	password = request.POST['password']
-
-	#check if username exists
-	if User.objects.filter(username=email).exists():
-		msg = Message.objects.get(text=Message.USERNAME_TAKEN)
-		HttpResponseRedirect(reverse('account:register_page') + '?msg=' + str(msg.id))
-
-	request.session['email'] = email
-	request.session['password'] = password
-
-	li = League_Import(LINK_PROFILE_CALLBACK)
-
-	request.session['request_token'] = li.get_request_token_str()
-
-	return HttpResponseRedirect(li.get_authorization_url())
+	return HttpResponseRedirect(reverse('account:link_profile'))
 
 @login_required
 def new_league(request):
@@ -58,31 +42,36 @@ def import_league_callback(request):
 	msg = 'Your league has been imported successfully! Your commissioner is: {}'.format(commish_team)
 	return render(request, 'account/dashboard.html', { 'success_msg' : msg })
 
-# @login_required
-# def link_profile(request):
+def link_profile(request):
+	if Manager.objects.filter(user=request.user).exists():
+		msg = "Your account is already linked to a Yahoo fantasy profile"
+		return render(request, 'account/dashboard.html',
+			{'info_msg' : msg})
 
+	li = League_Import(LINK_PROFILE_CALLBACK)
+
+	request.session['request_token'] = li.get_request_token_str()
+
+	return HttpResponseRedirect(li.get_authorization_url())
+
+@login_required
 def link_profile_callback(request):
 	li = League_Import(LINK_PROFILE_CALLBACK,
 		request.session['request_token'], request.GET['oauth_verifier'])
 
 	email = request.session['email']
 	password = request.session['password']
-	request.session['password'] = ''
 
 	User.objects.create_user(email, email, password)
+
 	user = authenticate(username=email, password=password)
 	login(request, user)
 
-	if Manager.objects.filter(user=request.user).exists():
-		return HttpResponseRedirect(reverse('account:dashboard'))
+	manager = li.get_or_create_manager(user)
 
-	try:
-		li.get_or_create_manager(user)
-	except Exception:
-		user.delete()
-		return HttpResponseRedirect(reverse('account:register'))
+	msg = 'Profile successfully Linked: {}'.format(manager.user.email)
 
-	return HttpResponseRedirect(reverse('account:dashboard.html'))
+	return render(request, 'account/dashboard.html', {'success_msg' : msg })
 
 @login_required
 def configure_invites(request, league_id):
