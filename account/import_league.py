0b00c9963c115b438f8e4bfc1ac255718d7cfccf
Brian Barton <brianjob@umich.edu>
2015-10-29 23:05:42 -0600

    num picks

--- account/import_league.py
+++ account/import_league.py
@@ -247,7 +247,7 @@ class League_Import(object):
 					src_team = league.team_set.get(yahoo_id=src_id)
 					dest_team = league.team_set.get(yahoo_id=dest_id)
 					
-					cleandump(pick['round'])
+					print 'NUM PICKS: ' + str(src_team.pick_set.count)
 					
 					pick = src_team.pick_set.filter(round=int(pick['round']))[0]
 					pick.team = dest_team
