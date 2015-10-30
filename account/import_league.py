980fc5fd88c8cd337856298e0fdc9e7e9db1116f
Brian Barton <brianjob@umich.edu>
2015-10-29 22:10:48 -0600

    log pick

--- account/import_league.py
+++ account/import_league.py
@@ -229,7 +229,7 @@ class League_Import(object):
 
 					src_team = league.team_set.get(yahoo_id=src_id)
 					dest_team = league.team_set.get(yahoo_id=dest_id)
-
+					print 'pick: ' + pick
 					pick = src_team.pick_set.filter(round=int(pick['round']))[0]
 					pick.team = dest_team
 					pick.save()
