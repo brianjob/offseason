1e6c94dc52b856a189b52b35c9ac5ce34ea46f16
Brian Barton <brianjob@umich.edu>
2015-11-18 20:30:13 -0700

    cancel pending trade conflicts edit

--- trades/views.py
+++ trades/views.py
@@ -301,12 +301,12 @@ def cancel_conflicting_trades(trade):
 	players = [p.player for p in trade.playerpiece_set.all()]
 	picks = [p.pick for p in trade.pickpiece_set.all()]
 
-	cancel_trades(trade.team1.trades_proposed)
-	cancel_trades(trade.team1.trades_received)
-	cancel_trades(trade.team2.trades_proposed)
-	cancel_trades(trade.team2.trades_received)
+	cancel_trades(trade.team1.trades_proposed, players, picks)
+	cancel_trades(trade.team1.trades_received, players, picks)
+	cancel_trades(trade.team2.trades_proposed, players, picks)
+	cancel_trades(trade.team2.trades_received, players, picks)
 
-def cancel_trades(trades):
+def cancel_trades(trades, players, picks):
 	for t in trades.filter(rejected_date=None).filter(completed_date=None).filter(vetoed_date=None):
 		if t == trade:
 			continue
